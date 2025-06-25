bf8bdfd168fd242308dfc33f043247dd0559cc43
ThibeauWouters <thibeauwouters@gmail.com>
2025-06-25 23:45:07 +0200

    Remove debug lines

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -506,13 +506,6 @@ class Sampler(object):
         =======
         list: Properly rescaled sampled values
         """
-        logger.info("Transforming prior samples to target space")
-        logger.info("DEBUG: theta")
-        logger.info(theta)
-        
-        logger.info("DEBUG: self._search_parameter_keys")
-        logger.info(self._search_parameter_keys)
-        
         return self.priors.rescale(self._search_parameter_keys, theta)
 
     def log_prior(self, theta):
@@ -600,15 +593,7 @@ class Sampler(object):
         while len(unit_cube) < npoints:
             print(f"{len(unit_cube)}/{npoints}")
             unit = rng.uniform(0, 1, self.ndim)
-            
-            print("unit")
-            print(unit)
-            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
             theta = self.prior_transform(unit)
-            
-            print("theta")
-            print(theta)
-            
             if self.check_draw(theta, warning=False):
                 print(f"Draw accepted: len(unit_cube) = {len(unit_cube)} / npoints = {npoints}")
                 unit_cube.append(unit)
@@ -640,12 +625,8 @@ class Sampler(object):
             True if the likelihood and prior are finite, false otherwise
 
         """
-        print("Computing log_prior")
         log_p = self.log_prior(theta)
-        print(f"log_prior: {log_p}")
-        print("Computing log_likelihood")
         log_l = self.log_likelihood(theta)
-        print(f"log_likelihood: {log_l}")
         
         print("Checking bad values")
         return self._check_bad_value(
